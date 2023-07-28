c1fd0ac668c8ebc5c4b0beab8251cb9d366f9ad5
MimHe <mimhe1381@gmail.com>
2023-07-28 21:14:38 +0330

    Complete RESTful API

---
+++ Express/RESTful_API.js
@@ -0,0 +1,75 @@
+const Joi = require('joi');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const courses = [
+    {id: 1, name: 'Course1'},
+    {id: 2, name: 'Course2'},
+    {id: 3, name: 'Course3'}
+];
+
+app.get('/', (req, res) => {
+    res.send('Home.');
+});
+
+// route parameters (essential/required data) => /:param1/:param2/...
+// query string parameters (additional data) => /:params?param1=name&param2=id$...
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === req.params.id);
+    if (!course) return res.status(404).send('The course with the given ID was not found.');
+    res.send(course);
+});
+// http://localhost:3000/api/courses/1000?sortBy=popularity => req.query: {"sortBy":"popularity"}
+
+app.post('/api/courses', (req, res) => {
+    const { error } = validateCourse(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course); // return what we post to client
+});
+
+app.put('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === req.params.id);
+    if (!course) return res.status(404).send('The course with the given ID was not found.');
+
+    const { error} = validateCourse(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    course.name = req.body.name;
+    res.send(course); // return updated course
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === req.params.id);
+    if (!course) return res.status(404).send('The course with the given ID was not found.');
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course); // return deleted course
+});
+
+/* Input Validation:
+    if (typeof req.body.name && req.body.name.length < 3 && !req.body.name) { // ... }
+*/
+// better way (using 'joi' package):
+function validateCourse(course) {
+    // schema: defines the shape of our object
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.valid(course, schema); // return: {error:null, value:{},...} / {error: ..., value: null,...}
+}
+
+// using environment variables
+require('dotenv').config();
+const port = process.env.PORT;
+app.listen(port, () => console.log(`Listening to port ${port}...`)); // port, callback?,...
+// -----> to call http services => 'Postman' app or extension
