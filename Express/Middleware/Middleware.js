c1d651c5987ee837e4f79792a9ac9f3b4be3fee8
MimHe <mimhe1381@gmail.com>
2023-08-04 19:01:25 +0330

    Update Middleware

--- Express/Middleware/Middleware.js
+++ Express/Middleware/Middleware.js
@@ -7,12 +7,16 @@ the response object (res), and the next middleware function in the applicationâ€
 The next middleware function is commonly denoted by a variable named next.
  */
 /*
+app.use([path,] callback [, callback...]):
+the middleware function is executed when the base of the requested path matches path.
+*/
+/*
 Middleware functions can perform the following tasks:
 * Execute any code.
 * Make changes to the request and the response objects.
 * End the request-response cycle.
 * Call the next middleware function in the stack.
- */
+*/
 
 // Creating custom Middleware
 app.use(function(req, res, next) {
