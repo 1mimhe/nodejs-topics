c89b7a4e4e9b34e537f230977f0227659ef5a76c
MimHe <mimhe1381@gmail.com>
2024-03-27 23:28:35 +0330

    update jwt

--- Auth/JWT/auth.js
+++ Auth/JWT/auth.js
@@ -1,5 +1,4 @@
 // Authentication is the process of identifying if the user is who they claim they are.
-const config = require('config');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const express = require('express');
@@ -9,8 +8,8 @@ const _ = require('lodash');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 
-// checking for environment variable existence
-if (!config.get('jwtPrivateKey')) {
+// checking for environment variable existence.
+if (!process.env.JWT_PRIVATE_KEY) {
     throw new Error('jwtPrivateKey is not defined.');
 }
 
@@ -59,21 +58,24 @@ const userSchema = new mongoose.Schema({
 // also, we can use 'pre' middleware to hash password
 // this middleware, execute a function before that method called.
 userSchema.pre('save', async function (next) {
-    /*
     const user = this;
 
     if (user.isModified('password')) {
         user.password = await bcrypt.hash(user.password, 10);
     }
-    */
     next();
 });
 
 userSchema.methods.generateAuthToken = function () {
-    return jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, config.get('jwtPrivateKey'));
+    return jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, process.env.JWT_PRIVATE_KEY);
+    // first-arg: payload
+    // second-arg: secretOrPrivateKey => will be used for create that digital signature.
+    // *** We put secret in an environment variable. We should not store it in our source code.
     // [options]:
-    // algorithm (default: HS256)
-    // expiresIn
+    // algorithm (default: HS256) is a symmetric-keyed hashing algorithm that uses one secret key.
+    // (Symmetric: means two parties share the secret key.
+    // The key is used for both generating the signature and validating it.)
+    // expiresIn => 1s, 50d, 2w, 1m, ...
     // notBefore
     // ...
 }
@@ -85,19 +87,15 @@ app.post('/api/register', async (req, res) => {
     let user = await User.findOne({ email: req.body.email });
     if (user) return res.status(400).send('User already registered.');
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
     user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: hashedPassword,
+        password: req.body.password,
         isAdmin: false
     });
 
     try {
         const result = await user.save();
-        // const token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey'));
         const token = user.generateAuthToken();
         res.header('x-auth-token', token).send(_.pick(result, ['_id', 'name', 'email']));
         // An HTTP header is a field of an HTTP request or response
@@ -117,17 +115,13 @@ app.post('/api/auth', async (req, res) => {
        const validPassword = await bcrypt.compare(req.body.password, user.password);
        if (!validPassword) return res.status(400).send('Invalid email or password.');
 
-       // first-arg: payload
-       // second-arg: secretOrPrivateKey => will be used for create that digital signature.
-       // *** We put secret in an environment variable. We should not store it in our source code.
-       // const token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey'));
        const token = user.generateAuthToken();
        res.send(token);
        /*
-       token:
        {
         "_id": ...
         "iat": the time the token was created.
+        ?exp: the time the token be expired.
        }
         */
    } catch (e) {
@@ -141,7 +135,7 @@ async function auth(req, res, next) {
     if (!token) return res.status(401).send('Access denied. No token provided.');
 
     try {
-        const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+        const decoded = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
         req.user = await User.findOne({ _id: decoded._id });
         next();
     } catch (e) {
